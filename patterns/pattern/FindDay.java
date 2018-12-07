18be247ce9b95ab002ba0f44d5b04c139d660609
Sushil-Hadiya <39803467+Sushil-Hadiya@users.noreply.github.com>
2018-12-07 20:21:19 +0530

    Incomplete

---
+++ patterns/pattern/FindDay.java
@@ -0,0 +1,20 @@
+package pattern;
+
+import java.util.Scanner;
+
+public class FindDay {
+
+	String day[] = { "Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat" };
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int d1 = sc.nextInt();
+		int d2 = sc.nextInt();
+		int odd = Math.abs(d1 - d2);
+		int c = 0;
+		if (odd % 4 == 0) {
+			
+		}
+	}
+
+}
